22e4171554438152dc3599fb7a2a0c3d0a1a6196
Ian Eure <ian@retrospec.tv>
2019-11-04 19:52:55 -0800

    Fix name

--- symon-cpu.el
+++ symon-cpu.el
@@ -6,55 +6,55 @@
 (defconst piemon-cpu--linux-path
   "/sys/devices/system/cpu/")
 
-(defun piemon-cpu--path (cpu-num &rest stats)
+(defun symon-cpu--path (cpu-num &rest stats)
   "Return the path for stat STAT on CPU CPU-NUM."
-  (concat (format "%scpu%d" piemon-cpu-path cpu-num)
+  (concat (format "%scpu%d" symon-cpu-path cpu-num)
           (when stats (concat "/" (c-concat-separated stats "/")))))
 
-(defun piemon--slurp-cpu (cpu-num &rest stats)
+(defun symon--slurp-cpu (cpu-num &rest stats)
   "Return value of stat STAT on CPU CPU-NUM."
-  (piemon--slurp-line (apply #'piemon-cpu--path cpu-num stats)))
+  (symon-monitor--slurp-line (apply #'symon-cpu--path cpu-num stats)))
 
-(defun piemon-cpu--range (high-maybe-low)
+(defun symon-cpu--range (high-maybe-low)
   "Turn range HIGH-MAYBE-LOW into a sequence of numbers."
   (if (not (string-match-p "-" high-maybe-low))
       (list (read high-maybe-low))
     (apply #'number-sequence (mapcar #'read (split-string high-maybe-low "-")))))
 
-(defun piemon-cpu--ranges (ranges)
+(defun symon-cpu--ranges (ranges)
   "Turn RANGES into a list of discrete numbers.
 
    RANGES is a comma-separated string of numbers or N-M,
    representing that sequence."
   (save-match-data
     (cl-loop for range in (split-string ranges ",")
-             append (piemon-cpu--range range))))
+             append (symon-cpu--range range))))
 
-(defun piemon-cpu--cpus ()
+(defun symon-cpu--cpus ()
   "Return (ONLINE-CPU-IDS . TOTAL-NUM-CPUS)."
   (cons
-   (thread-first (concat piemon-cpu-path "/online")
-     (piemon--slurp-line)
-     (piemon-cpu--ranges))
-   (thread-first (concat piemon-cpu-path "/possible")
-     (piemon--slurp-line)
-     (piemon-cpu--ranges)
+   (thread-first (concat symon-cpu-path "/online")
+     (symon--slurp-line)
+     (symon-cpu--ranges))
+   (thread-first (concat symon-cpu-path "/possible")
+     (symon--slurp-line)
+     (symon-cpu--ranges)
      (length))))
 
-(defun piemon-cpu--freq (cpu)
+(defun symon-cpu--freq (cpu)
   "Return the clock frequency of CPU."
   (cl-loop for stat in '("cpuinfo_min_freq" "cpuinfo_max_freq" "scaling_cur_freq")
-           collect (read (piemon--slurp-cpu cpu "cpufreq" stat))))
+           collect (read (symon--slurp-cpu cpu "cpufreq" stat))))
 
-(defun piemon-cpu--freqs (cpus)
+(defun symon-cpu--freqs (cpus)
   "Return the clock frequency information for CPUS."
-  (mapcar #'piemon-cpu--freq cpus))
+  (mapcar #'symon-cpu--freq cpus))
 
-(defun piemon-cpu--minfreq (&optional cpus)
-  (/ (reduce #'min (mapcar #'car (piemon-cpu--freqs (car (piemon-cpu--cpus))))) 1000.0))
+(defun symon-cpu--minfreq (&optional cpus)
+  (/ (reduce #'min (mapcar #'car (symon-cpu--freqs (car (symon-cpu--cpus))))) 1000.0))
 
-(defun piemon-cpu--maxfreq (&optional cpus)
-  (/ (reduce #'max (mapcar #'cadr (piemon-cpu--freqs (car (piemon-cpu--cpus))))) 1000.0))
+(defun symon-cpu--maxfreq (&optional cpus)
+  (/ (reduce #'max (mapcar #'cadr (symon-cpu--freqs (car (symon-cpu--cpus))))) 1000.0))
 
 (defclass symon-cpu-linux (symon-monitor-history)
   ((last-total-ticks :type integer :initform 0)
