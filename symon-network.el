906eaa0daf080e96e1b225ce972fb5e543b2dfe5
Ian Eure <ian@retrospec.tv>
2019-11-04 19:54:27 -0800

    Break out networking monitor.

---
+++ symon-network.el
@@ -0,0 +1,48 @@
+(require 'symon-monitor)
+(require 'symon-darwin)
+(require 'symon-windows)
+
+;; FIXME this is broken
+(defclass symon-linux-network-rx (symon-monitor-history)
+  ((default-display-opts
+     :type list
+     :initform '(:index "RX:" :unit "KB/s" :sparkline t
+                        :upper-bound 300
+                        :lower-bound 0))))
+
+(cl-defmethod symon-monitor-fetch ((this symon-linux-network-rx))
+  (let ((last-value (symon-monitor-value this)))
+    (with-temp-buffer
+      (insert-file-contents "/proc/net/dev")
+      (goto-char 1)
+      (let ((rx 0))
+        (while (search-forward-regexp "^[\s\t]*\\(.*\\):" nil t)
+          (unless (string= (match-string 1) "lo")
+            (setq rx (+ rx (read (current-buffer))))))
+        (prog1 (when last-value
+                 (/ (- rx last-value) symon-refresh-rate 1000))
+          (setf last-value rx))))))
+
+;; FIXME this is broken
+(defclass symon-linux-network-tx (symon-monitor-history)
+  ((default-display-opts
+     :type list
+     :initform '(:index "RX:" :unit "KB/s" :sparkline t
+                        :upper-bound 100
+                        :lower-bound 0))))
+
+(cl-defmethod symon-monitor-fetch ((this symon-linux-network-tx))
+  (let ((last-value (symon-monitor-value this)))
+    (with-temp-buffer
+      (insert-file-contents "/proc/net/dev")
+      (goto-char 1)
+      (let ((tx 0))
+        (while (search-forward-regexp "^[\s\t]*\\(.*\\):" nil t)
+          (unless (string= (match-string 1) "lo")
+            (forward-word 8)
+            (setq tx (+ tx (read (current-buffer))))))
+        (prog1 (when last-value
+                 (/ (- tx last-value) symon-refresh-rate 1000))
+          (setf last-value tx))))))
+
+(provide 'symon-network)
--- symon.el
+++ symon.el
@@ -155,52 +155,6 @@ monitor from:
              (get-buffer symon--process-buffer-name))
     (kill-buffer symon--process-buffer-name)))
 
-;; + predefined monitors
-;;   + linux monitors
-
-;; FIXME this is broken
-(defclass symon-linux-network-rx (symon-monitor-history)
-  ((default-display-opts
-     :type list
-     :initform '(:index "RX:" :unit "KB/s" :sparkline t
-                        :upper-bound 300
-                        :lower-bound 0))))
-
-(cl-defmethod symon-monitor-fetch ((this symon-linux-network-rx))
-  (let ((last-value (symon-monitor-value this)))
-    (with-temp-buffer
-      (insert-file-contents "/proc/net/dev")
-      (goto-char 1)
-      (let ((rx 0))
-        (while (search-forward-regexp "^[\s\t]*\\(.*\\):" nil t)
-          (unless (string= (match-string 1) "lo")
-            (setq rx (+ rx (read (current-buffer))))))
-        (prog1 (when last-value
-                 (/ (- rx last-value) symon-refresh-rate 1000))
-          (setf last-value rx))))))
-
-;; FIXME this is broken
-(defclass symon-linux-network-tx (symon-monitor-history)
-  ((default-display-opts
-     :type list
-     :initform '(:index "RX:" :unit "KB/s" :sparkline t
-                        :upper-bound 100
-                        :lower-bound 0))))
-
-(cl-defmethod symon-monitor-fetch ((this symon-linux-network-tx))
-  (let ((last-value (symon-monitor-value this)))
-    (with-temp-buffer
-      (insert-file-contents "/proc/net/dev")
-      (goto-char 1)
-      (let ((tx 0))
-        (while (search-forward-regexp "^[\s\t]*\\(.*\\):" nil t)
-          (unless (string= (match-string 1) "lo")
-            (forward-word 8)
-            (setq tx (+ tx (read (current-buffer))))))
-        (prog1 (when last-value
-                 (/ (- tx last-value) symon-refresh-rate 1000))
-          (setf last-value tx))))))
-
 ;; + symon core
 
 (defvar symon--active-monitors nil)
