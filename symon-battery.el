7345c24ce9b78d84290deb0c2e90506c0c2822f4
Ian Eure <ian@retrospec.tv>
2019-12-24 17:21:43 -0800

    Handle cases where the battery is discharging, but has no estimate.

--- symon-battery.el
+++ symon-battery.el
@@ -57,7 +57,14 @@ an hour's charge."
   "Face for when the battery is being charged."
   :group 'symon-battery)
 
-(defun symon-battery--face (charging time-left)
+(defun symon-battery--capacity-face (charging percent)
+  (if charging 'symon-battery-charging-face
+    (cond
+     ((>= percent 50) 'symon-battery-full-face)
+     ((>= percent 25) 'symon-battery-medium-face)
+     (t 'symon-battery-low-face))))
+
+(defun symon-battery--time-face (charging time-left)
   "Return monitor face based on CHARGING and TIME-LEFT.
 
 If the battery is being charged, returns
@@ -75,6 +82,11 @@ estimated time to depletion, returns `symon-battery-low-face'."
        ((and (= hh 0) (> mm 30)) 'symon-battery-medium-face)
        ((= hh 0) 'symon-battery-low-face)
        (t 'symon-battery-full-face)))))
+
+(defun symon-battery--face (charging percent time-left)
+  (if (string= "N/A" time-left)
+      (symon-battery--capacity-face charging percent)
+    (symon-battery--time-face charging time-left)))
 
 ;;;###autoload
 (defclass symon-battery (symon-monitor)
@@ -107,7 +119,7 @@ estimated time to depletion, returns `symon-battery-low-face'."
                   percent
                   (if (string= time-left "N/A")
                       "" (concat (symon-battery--indicator this charging) time-left)))
-        (propertize 'face (symon-battery--face charging time-left))))))
+        (propertize 'face (symon-battery--face charging percent time-left))))))
 
 (provide 'symon-battery)
 ;;; symon-battery.el ends here
