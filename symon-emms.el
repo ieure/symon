d8b8f51a35ad12e0aed34f54d8b4c12bd9c94f95
Ian Eure <ian@retrospec.tv>
2020-01-19 09:06:09 -0800

    Some EMMS fixes.

--- symon-emms.el
+++ symon-emms.el
@@ -26,56 +26,56 @@
 
 (require 'symon)
 
-(defgroup symon-monitor-emms nil
-  "Fixme"
+(defgroup symon-emms nil
+  "EMMS monitor for Symon."
   :group 'symon)
 
-(defface symon-monitor-emms-artist-face
+(defface symon-emms-artist-face
   '((t :inherit emms-browser-artist-face))
   "Face for artist names."
-  :group 'symon-monitor-emms)
+  :group 'symon-emms)
 
 
-(defface symon-monitor-emms-title-face
+(defface symon-emms-title-face
   '((t :inherit emms-playlist-track-face))
   "Face for track titles."
-  :group 'symon-monitor-emms)
+  :group 'symon-emms)
 
-(defface symon-monitor-emms-time-face
+(defface symon-emms-time-face
   '((t :inherit default))
   "Face for playing time."
-  :group 'symon-monitor-emms)
+  :group 'symon-emms)
 
-(defclass symon-monitor-emms (symon-monitor) nil)
+(defclass symon-emms (symon-monitor) nil)
 
-(cl-defmethod symon-monitor-fetch ((this symon-monitor-emms))
+(cl-defmethod symon-monitor-fetch ((this symon-emms))
   (when (and (featurep 'emms) (not emms-player-stopped-p))
     (emms-playlist-current-selected-track)))
 
-(defun symon-monitor-emms--merge (track)
+(defun symon-emms--merge (track)
   (unless (memq 'info-artist track)
     (push '(info-artist) track))
   (unless (memq 'info-title track)
     (push '(info-title) track))
   track)
 
-(defun symon-monitor-emms--display-file (track)
+(defun symon-emms--display-file (track)
   (let ((info-artist (cdr (assoc 'info-artist track)))
         (info-title (cdr (assoc 'info-title track)))
         (name (cdr (assoc 'name track))))
       (s-join " " (list
-               (propertize (s-trim emms-playing-time-string) 'face 'symon-monitor-emms-time-face)
+               (propertize (s-trim emms-playing-time-string) 'face 'symon-emms-time-face)
                (s-join " - "
-                       (or (cl-remove-if 'null (list (when info-artist (propertize info-artist 'face 'symon-monitor-emms-artist-face))
-                                                     (when info-title (propertize info-title 'face 'symon-monitor-emms-title-face))))
-                           (list (propertize (file-name-base name) 'face 'symon-monitor-emms-title-face))))))))
+                       (or (cl-remove-if 'null (list (when info-artist (propertize info-artist 'face 'symon-emms-artist-face))
+                                                     (when info-title (propertize info-title 'face 'symon-emms-title-face))))
+                           (list (propertize (file-name-base name) 'face 'symon-emms-title-face))))))))
 
 
-(defun symon-monitor-emms--display-url (track)
+(defun symon-emms--display-url (track)
   (let ((pu (url-generic-parse-url (cdr (assoc 'name track)))))
-    (propertize (url-host pu) 'face 'symon-monitor-emms-title-face)))
+    (propertize (url-host pu) 'face 'symon-emms-title-face)))
 
-(cl-defmethod symon-monitor-display ((this symon-monitor-emms))
+(cl-defmethod symon-monitor-display ((this symon-emms))
   (if-let ((track (symon-monitor-value this)))
     (concat
      (cond
@@ -84,8 +84,8 @@
       (t ""))
 
      (pcase (cdr (assoc 'type track))
-      ('file (symon-monitor-emms--display-file track))
-      ('url (symon-monitor-emms--display-url track))))))
+      ('file (symon-emms--display-file track))
+      ('url (symon-emms--display-url track))))))
 
 
 (provide 'symon-emms)
